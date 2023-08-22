e89d620dc0d3457f6b2d68170ead50c3c58958fd
Daniel Wang <daniel22373@gmail.com>
2023-08-22 17:21:25 -0400

    Implement view for posting review

--- backend/books/views.py
+++ backend/books/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
+from rest_framework import permissions
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+
+from .serializers import BookSerializer, ReviewSerializer
+
 # Create your views here.
+@api_view(['POST'])
+@permission_classes([permissions.IsAuthenticated])
+def post_review_view(request):
+    serializer = ReviewSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
