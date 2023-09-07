b19e438c0ccdf9fd8ed2a51201f4f71353356468
Daniel Wang <daniel22373@gmail.com>
2023-09-07 17:05:49 -0400

    Create services for user authentication and activities

---
+++ frontend/src/services/auth.service.js
@@ -0,0 +1,45 @@
+import axios from "axios";
+
+// URL for Django Rest API backend
+const API_URL = "http://127.0.0.1:8000/user/";
+
+class AuthService {
+  login(username, password) {
+    return axios
+        .post(API_URL + "token-auth/", {
+          username: username,
+          password: password
+        })
+        .then(response => {
+          if (response.data.token) {
+            // save user token and info in local storage
+            let userData = response.data;
+            userData['username'] = username;
+            userData = JSON.stringify(userData);
+            localStorage.setItem('user', userData);
+          }
+
+          return response.data;
+        });
+  }
+
+  logout() {
+    // delete user token and info from local storage
+    localStorage.removeItem('user');
+  }
+
+  register(email, username, password) {
+    return axios
+        .post(API_URL + "register/", {
+          email: email,
+          username: username,
+          password: password
+        });
+  }
+
+  getCurrentUser() {
+    return JSON.parse(localStorage.getItem('user'));
+  }
+}
+
+export default new AuthService();
---
+++ frontend/src/services/user.service.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+import authHeader from './auth-header';
+
+// URL for Django REST API backend
+const API_URL = 'http://127.0.0.1:8000/';
+
+class UserService {
+  /* insert functions to get, create, delete, etc. reviews here */
+}
+
+export default new UserService();
