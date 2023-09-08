f9cd4dd51fbb0d86cab3cd6e64ee14fa25a25ac0
Daniel Wang <daniel22373@gmail.com>
2023-09-08 16:33:32 -0400

    Write comments for admin.py

--- books/admin.py
+++ books/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
 from .models import Book, Review
 
+# Register Book and Review models to enable editing through Django admin interface
 admin.site.register(Book)
 admin.site.register(Review)
