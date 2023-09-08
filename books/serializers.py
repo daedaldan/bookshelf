11053e5bea0d8950a2d04e4993912756316fb06d
Daniel Wang <daniel22373@gmail.com>
2023-09-08 16:40:50 -0400

    Write comments for serializers.py

--- books/serializers.py
+++ books/serializers.py
@@ -2,19 +2,37 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import Book, Review
 
+# Serializers convert Django model instances into JSON that can be sent to the frontend through HTTPS requests.
+
 class UserSerializer(serializers.ModelSerializer):
+    """
+    This serializer converts the data for a User in Django to JSON.
+    """
     class Meta:
         model = User
-        fields = ('email', 'username', 'password', 'first_name', 'last_name')
+        # Serialize/deserialize the User's email, username, and password
+        fields = ('email', 'username', 'password')
+        # Set the password field to write_only to prevent exposure through API
         extra_kwargs = {'password': {"write_only": "True"}}
 
     def create(self, validated_data):
+        """
+        Creates a new User instance given username and password data from the frontend.
+
+        Parameters:
+        validated_data: The user info processed by the serializer.
+
+        Returns:
+        instance: The newly created instance of the User.
+        """
         password = validated_data.pop('password', None)
+        # Initialize the model instance with the validated data
         instance = self.Meta.model(**validated_data)
         if password is not None:
             instance.set_password(password)
 
         instance.save()
+
         return instance
 
 class BookSerializer(serializers.ModelSerializer):
@@ -26,6 +44,7 @@ class BookSerializer(serializers.ModelSerializer):
 
 class ReviewSerializer(serializers.ModelSerializer):
     id = serializers.ReadOnlyField()
+    # Include the username of the User who authored the Review in the serialized data
     owner = serializers.SlugRelatedField(queryset=User.objects.all(), slug_field="username")
 
     class Meta:
