aba59bc384aec343c72bd4d38ff1858f49054cb4
Daniel Wang <daniel22373@gmail.com>
2023-09-08 16:32:42 -0400

    Write comments for models.py

--- books/models.py
+++ books/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Book(models.Model):
-    """A book read by the user and their review of the book."""
+    """A book read by one or more users."""
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     year = models.PositiveIntegerField()
@@ -12,12 +12,14 @@ class Book(models.Model):
     def __str__(self):
         return "'" + self.title + "'" + " by " + self.author
 
+
 class Review(models.Model):
-    """The user's review of a book."""
+    """A user's review of a book."""
     title = models.CharField(max_length=200)
     description = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
 
+    # Link the Review to the User who wrote it and the Book it is for
     owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     book = models.ForeignKey(Book, on_delete=models.SET_NULL, null=True)
 
