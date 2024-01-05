c7afed997a7bcc433d56eb1d0a567015b13ed004
Daniel Wang <daniel22373@gmail.com>
2024-01-04 20:02:27 -0500

    Work on CSS for forms and navbar

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -6,8 +6,8 @@ import React from 'react';
  */
 export default function LandingPage() {
   return (
-      <div>
-        <h1 id="landing-page-heading">Find your next read</h1>
+      <div id="landing-page">
+        <h1 id="landing-page-heading">Find your next read.</h1>
         <p id="landing-page-text">Join a community of readers and discover the insights others have gained from their favorite books.</p>
         <footer>
           Daniel Wang, 2023
